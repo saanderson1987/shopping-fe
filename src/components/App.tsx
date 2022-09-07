92a44fe180795fc1388fff811923ef0996afe2da
Steven Anderson <saanderson1987@gmail.com>
2022-09-06 20:48:24 -0400

    mutation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import CreateLink from "./CreateLink";
 import LinkList from "./LinkList";
 
 function App() {
-  return <LinkList />;
+  return (
+    <>
+      <LinkList />
+      <CreateLink />
+    </>
+  );
 }
 
 export default App;
