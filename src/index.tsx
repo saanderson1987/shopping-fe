751d2fe4c7e00fe04475f7e59eab53a2b3a6720d
Steven Anderson <saanderson1987@gmail.com>
2022-09-08 20:57:13 -0400

    auth

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,29 @@ import {
   createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import { AUTH_TOKEN } from "./constants";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
