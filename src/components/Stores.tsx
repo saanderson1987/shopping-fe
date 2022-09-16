0f7aed839d9cd27a1541a5c2f36a33031c3535f5
Steven Anderson <saanderson1987@gmail.com>
2022-09-16 19:40:37 -0400

    table

--- src/components/Stores.tsx
+++ src/components/Stores.tsx
@@ -1,10 +1,56 @@
 import React from "react";
-import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-const Stores = ({}) => {
-  const t = "hi";
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  return { name, calories, fat, carbs, protein };
+}
 
-  return <Button variant="contained">Hello World</Button>;
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+const Stores = () => {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="stores" size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Neighborhood</TableCell>
+            <TableCell>Tags</TableCell>
+            <TableCell>Address</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name} sx={{ "&:last-child td": { border: 0 } }}>
+              {/* <TableCell component="th" scope="row"> */}
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.calories}</TableCell>
+              <TableCell>{row.fat}</TableCell>
+              <TableCell>{row.carbs}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default Stores;
