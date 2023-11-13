7708d92d41e85c86d21086e1a5a1d226886d1cf0
Steven Anderson <saanderson1987@gmail.com>
2023-11-12 21:32:47 -0500

    try marker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,14 @@
 // @ts-nocheck
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  MarkerF,
+  InfoWindowF,
+  InfoWindow,
+} from "@react-google-maps/api";
 import Typography from "@mui/material/Typography";
 
 const containerStyle = {
@@ -11,6 +17,8 @@ const containerStyle = {
 };
 
 const center = { lat: 40.72283097424528, lng: -74.00074286846672 };
+
+const markers = [{ lat: 40.7240352, lng: -74.0003392 }];
 
 const Container = ({ children }) => (
   <Box
@@ -30,6 +38,8 @@ const Map = () => {
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
   });
+
+  const [infoWindowAnchor, setInfoWindowAnchor] = useState(null);
 
   if (isLoaded) {
     return (
@@ -85,8 +95,24 @@ const Map = () => {
         }}
         zoom={15.5}
       >
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
+        {markers.map(({ lat, lng }) => (
+          <MarkerF
+            key={`${lat}-${lng}`}
+            onClick={(e) => {
+              console.log(e);
+              setInfoWindowAnchor(e);
+            }}
+            position={{ lat, lng }}
+          ></MarkerF>
+        ))}
+        {infoWindowAnchor && (
+          <InfoWindowF
+            anchor={infoWindowAnchor}
+            onCloseClick={setInfoWindowAnchor(null)}
+          >
+            Hi
+          </InfoWindowF>
+        )}
       </GoogleMap>
     );
   }
