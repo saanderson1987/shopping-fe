2f916f708a10fad785ec3cf8e7dd765caff6fe8e
Steven Anderson <saanderson1987@gmail.com>
2023-11-27 06:41:51 -0500

    query

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,35 @@
 import React, { useState, useCallback } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 import Map from "./Map/Map";
 import { Marker } from "./Map/create-markers";
 import Bottom from "./Bottom";
+import { useQuery, gql } from "@apollo/client";
+import { Typography } from "@mui/material";
 
 const markers = [
   { lat: 40.7240352, lng: -74.0003392, name: "Etro", storeId: "123" },
 ];
 
+const QUERY = gql`
+  query Query($filter: String) {
+    stores(filter: $filter) {
+      stores {
+        id
+        name
+        neighborhood {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
 const Main = () => {
+  const { data, error, loading } = useQuery(QUERY, {
+    variables: {},
+  });
+
   const [idStoreOpen, setIdStoreOpen] = useState<string | null>(null);
 
   const onClickMap = useCallback(() => {
@@ -22,6 +44,11 @@ const Main = () => {
     },
     [setIdStoreOpen]
   );
+
+  if (loading) return <CircularProgress />;
+  if (error) return <Typography>Error loading page.</Typography>;
+
+  console.log(data);
 
   return (
     <>
